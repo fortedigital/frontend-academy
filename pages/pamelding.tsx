88953361cee1fbc3605f0fe226e748495d0de332
Eric Veliyulin <eric.vel@outlook.com>
2022-11-23 14:44:29 +0100

    Create signup and resources pages

---
+++ pages/pamelding.tsx
@@ -0,0 +1,8 @@
+export default function SignUp() {
+  return (
+    <div className="">
+      <h1 className="mb-6 text-2xl font-bold">Påmelding til akademiet</h1>
+      <p className="">Skjema/lenke</p>
+    </div>
+  );
+}

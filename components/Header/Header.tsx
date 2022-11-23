1d39e29ccde9b1ec1d0e44940f97a3189cc70b8e
Eric Veliyulin <eric.vel@outlook.com>
2022-11-23 14:05:41 +0100

    Add Forte logo to Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import React from "react";
 
+import forteLogo from "assets/Forte_Symbol_white.svg";
+import Image from "next/image";
+
 interface HeaderLinkProps {
   href: string;
   children: React.ReactNode;
@@ -12,11 +15,18 @@ const HeaderLink = ({ href, children }: HeaderLinkProps) => (
   </Link>
 );
 
+const Logo = () => (
+  <Link href="/" className="flex items-center gap-x-3 py-2">
+    <Image src={forteLogo} alt="" className="w-8" />
+    <span className="text-lg font-bold">Frontend Academy</span>
+  </Link>
+);
+
 function Header() {
   return (
-    <div className="flex h-14 flex-none items-center justify-between gap-x-4 px-4">
-      <p className="font-bold">Forte Frontend Academy</p>
-      <div className="flex gap-x-4">
+    <div className="container mx-auto flex h-20 flex-none items-center justify-between gap-x-8 px-4">
+      <Logo />
+      <div className="flex gap-x-8">
         <HeaderLink href="/pamelding">Påmelding</HeaderLink>
         <HeaderLink href="/ressurser">Ressurser</HeaderLink>
       </div>

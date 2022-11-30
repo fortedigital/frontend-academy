82b8d541d8f6437b2c432aa77eb2bb9a56884f7e
Eric Veliyulin <eric.vel@outlook.com>
2022-11-30 13:30:10 +0100

    Add link to signup form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 import Icon from "components/Icon";
 
 export default function Home() {
@@ -7,17 +5,19 @@ export default function Home() {
     <div className="flex h-full flex-col justify-center">
       <div className="mx-auto flex max-w-3xl flex-col items-center justify-center">
         <h1 className="mb-14 text-center text-4xl font-bold md:mb-20 md:text-6xl">
-          Velkommen til <br /> Frontend Academy
+          Velkommen til <br /> Forte Frontend Academy
         </h1>
         <p className="mb-14 text-center md:text-lg">
           Ønsker du å utvikle deg enda mer innenfor frontendteknologier,
           deriblant universell utforming, kvalitet, sikkerhet og
-          konsulentvirksomhet? Meld deg på Frontendakademiet der vi utover våren
-          arrangerer 8 til 10 ettermiddager med kurs, workshops og
+          konsulentvirksomhet? Meld deg på Forte Frontend Academy, der vi utover
+          våren arrangerer 8 til 10 ettermiddager med kurs, workshops og
           gruppeoppgaver.
         </p>
-        <Link
-          href="/pamelding"
+        <a
+          href="https://forms.office.com/Pages/ResponsePage.aspx?id=nv06Bstf0kinacoxsPW0Qyz1_fHV7sxIpo8188t8HX5UOEZHRFRBMEg0S1VERTY4OUtWTzhFRlZIQyQlQCN0PWcu"
+          target="_blank"
+          rel="noopener noreferrer"
           className="group flex h-10 items-center gap-x-2 rounded-sm bg-red-600 pl-3 pr-5 font-bold transition-colors hover:bg-red-700 active:bg-red-800"
         >
           Meld deg på
@@ -25,7 +25,7 @@ export default function Home() {
             name="ArrowRightIcon"
             className="transition-transform group-hover:translate-x-2"
           />
-        </Link>
+        </a>
       </div>
     </div>
   );

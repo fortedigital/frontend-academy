2244b0fba6f985891cc5dffed811c55a0a7c3487
Lana Vu <lana.vu@obos.no>
2023-01-18 14:41:12 +0100

    Update plan

--- pages/plan.tsx
+++ pages/plan.tsx
@@ -1,16 +1,48 @@
 export default function Plan() {
   const planData = [
     {
-      date: "01.01.23",
+      date: "02. februar 2023",
       topic: "Introduksjon",
     },
     {
-      date: "01.01.23",
-      topic: "Introduksjon",
+      date: "16. februar 2023",
+      topic: "Hva leverer Forte? Verdier og strategier",
     },
     {
-      date: "01.01.23",
-      topic: "Introduksjon",
+      date: "02. mars 2023",
+      topic: "Faggruppesamling",
+    },
+    {
+      date: "16. mars 2023",
+      topic: "Kvalitetssikring på høyt nivå",
+    },
+    {
+      date: "xx. april 2023",
+      topic: "Universell utforming v. Kristoffer Lium (NRK)",
+    },
+    {
+      date: "xx. april 2023",
+      topic: "Faggruppesamling",
+    },
+    {
+      date: "xx. mai 2023",
+      topic: "Sikkerhet",
+    },
+    {
+      date: "xx. mai 2023 (Etter arbeidstid med sosialt)",
+      topic: "Autentisering",
+    },
+    {
+      date: "xx. mai 2023",
+      topic: "TBD",
+    },
+    {
+      date: "xx. juni 2023",
+      topic: "Faggruppesamling",
+    },
+    {
+      date: "xx. juni 2023",
+      topic: "Avsluttende oppgave",
     },
   ];
 
